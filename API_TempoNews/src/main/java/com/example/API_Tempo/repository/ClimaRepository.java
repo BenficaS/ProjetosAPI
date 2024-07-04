5d11fc8b264d93418c2ed64de12d44316384b870
BenficaS <107698702+BenficaS@users.noreply.github.com>
2024-07-03 22:29:46 -0300

    Projetos API_Rest e API_Restfull

---
+++ API_TempoNews/src/main/java/com/example/API_Tempo/repository/ClimaRepository.java
@@ -0,0 +1,13 @@
+package com.example.API_Tempo.repository;
+
+import com.example.API_Tempo.model.ClimaEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClimaRepository extends MongoRepository<ClimaEntity, String> {
+// Métodos de CRUD já estão disponíveis
+//findAll, findById, save, deleteById
+}
